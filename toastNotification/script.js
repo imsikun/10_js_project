d9d3e237db4e4dcab4347856b102639854613759
Subh <subhakant@staragrifinance.com>
2020-09-12 11:33:33 +0530

    toast notification completed

---
+++ toastNotification/script.js
@@ -0,0 +1,19 @@
+const btn = document.getElementById('btn')
+const container = document.getElementById('container')
+
+btn.addEventListener('click', () => {
+  createNotification()
+})
+
+function createNotification () {
+  const notification = document.createElement('div')
+  notification.classList.add('toast')
+
+  notification.innerText = 'This is a crazy and resourceful challenge'
+
+  container.appendChild(notification)
+
+  setTimeout(() => {
+    notification.remove()
+  }, 3000)
+}
